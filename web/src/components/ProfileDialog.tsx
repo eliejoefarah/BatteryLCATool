98ae40dc7931cd240f3390f480469fa7cd82fc2d
eliejoefarah <eliejoefarah@gmail.com>
2026-03-06 11:11:58 +0100

    ci frontend fixes 6

--- web/src/components/ProfileDialog.tsx
+++ web/src/components/ProfileDialog.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { CalendarDays, FlaskConical, FolderOpen, GitBranch, ShieldCheck, User } from 'lucide-react'
 import { supabase } from '../lib/supabase'
